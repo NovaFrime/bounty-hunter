761a220ba51bbd4cc61497b8eadc51387c570c42
NovaFrost <holyprime1985@gmail.com>
2023-12-03 08:03:56 +0700

    Update crawl-data.js

--- crawl-data.js
+++ crawl-data.js
@@ -17,27 +17,27 @@ async function fetchDataShopee() {
  fs.writeFileSync('data.json', JSON.stringify(allData, null, 2));
 }
 fetchDataShopee();
-// async function crawlPage(url) {
-//   const browser = await puppeteer.launch({headless: "new"});
-//   const page = await browser.newPage();
-//   await page.goto(url);
-//   const data = await page.evaluate(() => {
-//     let results = [];
-//     document.querySelectorAll("#__next > div.hot-deal-page > main > div > div > div:nth-child(7) > div").forEach(element => {
-//       let text = element.querySelector('div.title').textContent;
-//       results.push(text);
-//     });
-//     return results;
-//   });
-//   await browser.close();
-//   return data;
-//  }
+async function crawlPage(url) {
+  const browser = await puppeteer.launch({headless: "new"});
+  const page = await browser.newPage();
+  await page.goto(url);
+  const data = await page.evaluate(() => {
+    let results = [];
+    document.querySelectorAll("#__next > div.hot-deal-page > main > div > div > div:nth-child(7) > div > div.List__Wrapper-sc-1ap7nsk-0.cEBRuU.styles__FlashDealItemList-sc-1466pn8-4.hvXODY").forEach(element => {
+      let text = element.querySelector('div.styles__Wrapper-sc-6jfdyd-0.iNsGak').textContent;
+      results.push(text);
+    });
+    return results;
+  });
+  await browser.close();
+  return data;
+ }
 
 
-// crawlPage('https://tiki.vn/deal-hot?tab=now')
-//   .then(results => {
-//       console.log(results);
-//   })
-//   .catch(error => {
-//       console.error(error);
-//   });
+crawlPage('https://tiki.vn/deal-hot?tab=now')
+  .then(results => {
+      console.log(results);
+  })
+  .catch(error => {
+      console.error(error);
+  });
